e35579f9eeff08798e0a711e1a649b21bb7ac647
cullfy <cullfydev@gmail.com>
2023-09-15 02:57:29 +0300

    fix: add allowed origins validation

--- src/utils/headers.ts
+++ src/utils/headers.ts
@@ -2,15 +2,20 @@ import { Response } from 'express';
 
 const __ALLOWED_ORIGINS__: string[] = [
     'https://rasp.cullfy.ru',
-    'https://dev.cullfy.ru',
+    'http://dev.cullfy.ru',
+    'http://45.12.72.131'
 ]
+
+const validateOrigin = (origin: string): string => {
+    return __ALLOWED_ORIGINS__.includes(origin) ? '*' : '';
+}
 
  export const setHeaders = (res:  Response) => {
     res.set({
-        'Access-Control-Allow-Origin': __ALLOWED_ORIGINS__.join(', '),
         'Access-Control-Allow-Methods': 'GET',
         'Access-Control-Allow-Headers': 'Access-Control-Allow-Origin,' +
         ' Content-Type, Access-Control-Allow-Methods, Access-Control-Request-Headers,' +
         ' Access-Control-Allow-Headers, Accept'
     });
+    res.setHeader('Access-Control-Allow-Origin', validateOrigin(res.get('origin') || ''));
 }
