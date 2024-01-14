798a3990849631e3ffdc4f51720eec8b4061a9b8
Grigoriy Gorbovskoy Vladimirovich <grishaxtail@gmail.com>
2024-01-14 17:40:22 +0300

    fix: удалил build скрипт

--- src/server.ts
+++ src/server.ts
@@ -45,12 +45,9 @@ fastify.get('/', async () => {
   return { hello: 'world' }
 });
 
-/**
- * Run the server!
- */
 const start = async () => {
   try {
-    await fastify.listen({ port: Number(process.env['PORT']) ?? 3000 })
+    await fastify.listen({ port: Number(process.env['PORT']) ?? 3000, host: '0.0.0.0' })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
